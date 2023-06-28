2c8f8347e123078817e58e49497117a2283601ca
micahwaweru <micah.waweru@moody.edu>
2023-06-28 15:21:19 -0500

    start firebase connection - switching computers

--- app/screens/List.tsx
+++ app/screens/List.tsx
@@ -1,6 +1,15 @@
 import { View, Text, Button } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { addDoc, collection } from 'firebase/firestore'
+import { FIRESTORE_DB } from '../../firebaseConfig'
 
+useEffect(()=>{
+})
+
+const addWorkout = async ()=>{
+    const doc = addDoc(collection(FIRESTORE_DB, 'workouts'), {title: 'I am a test', description: 'I am testing'})
+    console.log('File: List.tsx:12 ~ addTodo ~ doc:', doc)
+};
 const List = ({ navigation }: any) => {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
