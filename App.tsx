d7b3747d7bf9da91aafd7d6ecc626c3bd54cec37
micahwaweru <micah.waweru@moody.edu>
2023-06-28 11:41:35 -0500

    set up react navigator

--- App.tsx
+++ App.tsx
@@ -4,28 +4,24 @@ import { StyleSheet, Text, View } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import List from './app/screens/List';
+import Workouts from './app/screens/Workouts';
 
-const copy = "Hello world"
+
 
 
 
 const Stack = createNativeStackNavigator();
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
+
 
 
 export default function App() {
   return (
     
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name='Home' component={HomeScreen}></Stack.Screen>
+        <Stack.Navigator initialRouteName='Home'>
+          <Stack.Screen name='Home' component={List} options={{title: 'Home'}}></Stack.Screen>
+          <Stack.Screen name='Workouts' component={Workouts} options={{title: 'Overview'}}></Stack.Screen>
         </Stack.Navigator>
       </NavigationContainer>
   )
--- app/screens/List.tsx
+++ app/screens/List.tsx
@@ -1,11 +1,13 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
 
-const List = () => {
+const List = ({ navigation }: any) => {
   return (
-    <View>
-      <Text>List</Text>
-    </View>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <Text>Home Screen</Text>
+    <Button onPress={() => navigation.navigate('Workouts')} title='Open Workouts'/>
+    
+  </View>
   )
 }
 
